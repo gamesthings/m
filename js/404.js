0342754f49fa990652dea65b7af7654cea683a28
Cory Collier <cory.collier@waitrapp.com>
2021-05-27 22:09:47 -0400

    adding source script

--- js/404.js
+++ js/404.js
@@ -1,10 +1,5 @@
-document.onload = function() {
-    // iterate over any item with the 'redX' class attribute
-    [...document.getElementsByClassName('redX')]
-        .forEach(function(item){
-            item.addEventListener('click', function(){
-                document.getElementById('popup').remove();
-            })
-        });
 
-}
+document.getElementById('close-button')
+.addEventListener('click', function(){
+    document.getElementById('popup').remove();
+})
